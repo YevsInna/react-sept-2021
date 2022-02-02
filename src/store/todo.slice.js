00192815defeb2e23f84b5f4f5897ba48aa4ceae
YevsInna <evseinka@gmail.com>
2022-02-02 19:58:55 +0200

    cw-redux-todo

--- src/store/todo.slice.js
+++ src/store/todo.slice.js
@@ -9,13 +9,16 @@ const todoSlice = createSlice({
     name: 'todoSlice',
     initialState,
     reducers: {
-        //тут прописуються actions: що і як буде робитись
-        addTodo:(state,action)=>{
-
+        //тут прописуються actions: що і як буде робитись, їх обз експортувати внизу файла
+        addTodo: (state, action) => {
+            //в масив тудушок додаємо обєкт з айді, неймом(дані з форми) і статусом(по дефолту як невиконане)
+            state.todos.push({id: new Date().getTime(), name: action.payload.todo, status: false})
         }
     }
 });
 
 // із слайса витягуємо редьюсер:
 const todoReducer = todoSlice.reducer;
+
+export const {addTodo} = todoSlice.actions;
 export default todoReducer;
