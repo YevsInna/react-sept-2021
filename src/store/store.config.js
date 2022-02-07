98d6733eeefcb6162faa78a9b905239c295adcc0
YevsInna <evseinka@gmail.com>
2022-02-07 13:06:49 +0200

    redux: cars, users, posts, comments

--- src/store/store.config.js
+++ src/store/store.config.js
@@ -1,11 +1,15 @@
 import {configureStore} from "@reduxjs/toolkit";
 import carReducer from "./car.slice";
 import usersReducer from "./users.slice";
+import postsReducer from "./posts.slice";
+import commentsReducer from "./comments.slice";
 
 const store = configureStore({
     reducer: {
         carReducer,
-        usersReducer
+        usersReducer,
+        postsReducer,
+        commentsReducer
     }
 });
 export default store
